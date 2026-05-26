ef4b368f12115bebd6cac92c9b03eba0b0e2feaa
pmh-1008 <mhangpham0908@gmail.com>
2026-05-26 07:51:44 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,6 +20,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool checkInput(int l, int k, string hStr, string mStr)
+{
+    if (k >= l)
+    {
+        cout << "Vi pham rang buoc: l > k";
+        return false;
+    }
+
+    if (hStr.size() != k + 1)
+    {
+        cout << "Vi pham rang buoc: hStr phai co k + 1 bit";
+        return false;
+    }
+
+    GF2Poly hx = GF2Poly::fromBitString(hStr);
+    GF2Poly xl1 = GF2Poly::monomial(l) + GF2Poly({1});
+    auto [g, rem] = GF2Poly::divmod(xl1, hx);
+    if (!rem.isZero())
+    {
+        cout << "Vi pham rang buoc: x^l+1 chia het cho h(x)";
+        return false;
+    }
+
+    if (mStr.size() != k)
+    {
+        cout << "Vi pham rang buoc: mStr phai co k + 1 bit";
+        return false;
+    }
+    
+    return true;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -27,6 +59,9 @@ int main() {
     int l, k;
     string hStr, mStr;
     cin >> l >> k >> hStr >> mStr;
+
+    if (!checkInput(l, k, hStr, mStr))
+        return 0;
 
     const int r = l - k;
     GF2Poly hx = GF2Poly::fromBitString(hStr);
